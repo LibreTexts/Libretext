3cc563f3dd0d7b70dea48a46410ea95579e57ee1
Miniland1333 <henryd.agnew@gmail.com>
2018-05-05 11:14:05 -0700

    Finished HTMLtoJSON

---
+++ public/TOC/TOC.js
@@ -0,0 +1,45 @@
+function getChildren(HTML) {
+	let JSON = {};
+	if(HTML.children[1]) {
+		HTML = HTML.children[1].children;
+
+		for (let i = 0; i < HTML.length; i++) {
+			let link = HTML[i].children[0];
+			JSON[link.textContent] = {link: link.href, children: getChildren(HTML[i])};
+		}
+	}
+	return JSON;
+}
+
+(function HTMLtoJSON() {
+	let HTML = $("#treeHolder .wiki-tree");
+	let JSON = {};
+	let URL = window.location.href;
+
+	//get content root
+	URL = URL.split("/").slice(0, 6).join("/");
+	if (window.location.href === URL) {
+		//at content root already
+	}
+	HTML = HTML[0].children[0].children;
+	let link;
+
+	//Find Current Libretext
+	for (let i = 0; i < HTML.length; i++) {
+		link = HTML[i].children[1].children;
+
+		for (let j = 0; j < link.length; j++) {
+			if (link[j].children[0].href === URL) {
+				HTML = link[j].children[1].children;
+				break;
+			}
+		}
+	}
+
+	for (let i = 0; i < HTML.length; i++) {
+		link = HTML[i].children[0];
+		JSON[link.textContent] = {link: link.href, children: getChildren(HTML[i])};
+	}
+
+	console.log(JSON);
+}());
