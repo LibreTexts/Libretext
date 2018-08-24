cafc73ffee9f9ae0ad89330dbb0086f233933cb5
Miniland1333 <henryd.agnew@gmail.com>
2018-08-24 15:31:32 -0700

    Updated .travis.yml

---
+++ nodePrint/test.js
@@ -0,0 +1,22 @@
+const expect = require('Chai').expect;
+require('./nodePrint');
+const request = require('request');
+
+describe('basic response', function () {
+	customTest('should return 404', '', 404);
+	customTest('should return 200', 'logo.png', 200);
+});
+
+describe('dynamic response', function () {
+	customTest('should return 200', 'print/url=https://chem.libretexts.org/', 200);
+});
+
+function customTest(string, path, status) {
+	const port = 3001;
+	it(string, function (done) {
+		request.get(`http://localhost:${port}/${path}`, function (err, res, body) {
+			expect(res.statusCode).to.equal(status);
+			done();
+		});
+	});
+}
