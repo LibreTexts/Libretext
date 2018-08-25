4de54c858abe6d19e07509611b03d4791c1332e4
Miniland1333 <henryd.agnew@gmail.com>
2018-08-24 18:00:29 -0700

    Fixed timeout

--- nodePrint/test.js
+++ nodePrint/test.js
@@ -2,10 +2,12 @@ const stop = require('./nodePrint');
 const expect = require('chai').expect;
 const request = require('request');
 
-before(function(){
-	console.log("1");
-	this.timeout(10000);
-	console.log("2");
+before(function(done){
+	setTimeout(function () {
+		console.log("Done!");
+		done();
+	}, 5000);
+	this.timeout(20000);
 });
 
 describe('basic response', function () {
@@ -14,7 +16,7 @@ describe('basic response', function () {
 });
 
 describe('dynamic response', function () {
-	this.timeout(15000);
+	this.timeout(20000);
 	customTest('should return 200', 'print/url=https://chem.libretexts.org/?no-cache', 200);
 	customTest('should return 200', 'print/url=https://chem.libretexts.org/', 200);
 
@@ -28,10 +30,13 @@ function customTest(string, path, status) {
 	const port = 3001;
 	it(string, function (done) {
 		request.get(`http://localhost:${port}/${path}`, function (err, res, body) {
-			if (err)
+			if (err) {
+				console.error(err);
 				expect(false).to.equal(true);
-			else
+			}
+			else {
 				expect(res.statusCode).to.equal(status);
+			}
 			done();
 		});
 	});
