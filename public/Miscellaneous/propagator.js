0aeaeaa76e570bf7a907bf9170e7cd3e324d3f1f
Unknown <henryd.agnew@gmail.com>
2018-10-16 18:48:53 -0700

    Propagator 2

--- public/Miscellaneous/propagator.js
+++ public/Miscellaneous/propagator.js
@@ -17,19 +17,37 @@ class propagator {
 		this.reset();
 		let url = propagator.checkURL();
 		if (url) {
-			const subdomain = url.split(".")[0];
-			if (this.libraries.includes(subdomain)) {
-				let otherArray = ["bio", "biz", "chem", "eng", "geo", "human", "math", "med", "photon", "phys", "socialsci", "stats"];
+			const subdomain = url.split("/")[2].split(".")[0];
+			let otherArray = ["bio", "biz", "chem", "eng", "geo", "human", "math", "med", "photon", "phys", "socialsci", "stats"];
+			if (otherArray.includes(subdomain)) {
 				let index = otherArray.indexOf(subdomain);
 				if (index > -1) {
-					otherArray = otherArray.splice(index, 1);
-					let promiseArray = [];
-					let path = url.split("/")[1];
+					otherArray.splice(index, 1);
+					let path = url.split("/").slice(3).join("/");
+
+					//Loader
+					this.reset();
 					for (let i = 0; i < otherArray.length; i++) {
-						await loader(otherArray[i]);
-						promiseArray.push(propagator.propagatePage(otherArray[i], path));
+						this.loader(otherArray[i], "Waiting for Content...");
+					}
+
+					//Get Contents
+					let content = await propagator.getContent(subdomain, path);
+
+					//Propagatate
+					let promiseArray = [];
+					this.reset();
+					const authen = atob(document.getElementById("user").value + ":" + document.getElementById("password").value);
+					for (let i = 0; i < otherArray.length; i++) {
+						this.loader(otherArray[i], "Propagating...");
+						promiseArray.push(propagator.propagatePage(otherArray[i], path, content, authen));
 					}
 					await Promise.all(promiseArray);
+
+					// this.reset();
+					for (let i = 0; i < otherArray.length; i++) {
+						// this.loader(otherArray[i], "Complete!");
+					}
 				}
 			}
 		}
@@ -38,10 +56,37 @@ class propagator {
 		}
 	}
 
-	static async propagatePage(subdomain, path) {
+	static async getContent(subdomain, path) {
+		let content = await fetch("https://" + subdomain + ".libretexts.org/@api/deki/pages/=" + encodeURIComponent(encodeURIComponent(path)) + "/contents?mode=raw");
+		content = await content.text();
+		content = content.match(/(?<=<body>)([\s\S]*?)(?=<\/body>)/)[1];
+		return decodeHTML(content);
+
+		function decodeHTML(content) {
+			let ret = content.replace(/&gt;/g, '>');
+			ret = ret.replace(/&lt;/g, '<');
+			ret = ret.replace(/&quot;/g, '"');
+			ret = ret.replace(/&apos;/g, "'");
+			ret = ret.replace(/&amp;/g, '&');
+			return ret;
+		}
 	}
 
-	static async loader(subdomain) {
+	static async propagatePage(subdomain, path, content, authenticate) {
+		this.loader(subdomain, content);
+
+		let response = await fetch(subdomain + "/@api/deki/pages/=" + encodeURIComponent(encodeURIComponent(path + child.relativePath)) + "/contents", {
+			method: "POST",
+			body: content,
+			headers: {"Authentication": "Basic "}
+		});
+	}
+
+	static loader(subdomain, status) {
+		let thing = document.createElement("div");
+		thing.id = "results" + subdomain;
+		thing.innerHTML = `<img src="https://static.libretexts.org/img/LibreTexts/glyphs_blue/${subdomain}.png"><div>${status}</div>`;
+		document.getElementById("copyResults").appendChild(thing);
 	}
 
 	static checkURL() {
@@ -56,7 +101,7 @@ class propagator {
 	}
 
 	static reset() {
-
+		document.getElementById("copyResults").innerHTML = "";
 	}
 }
 
