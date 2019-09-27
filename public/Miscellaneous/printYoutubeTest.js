1b1710659fb5f1a8f6f2d13c6867cf4dcc88d80a
LadyofLothhlorien <clpera@ucdavis.edu>
2019-09-26 22:14:29 -0700

    QR Code generated
    It's not automated yet, but with a lot of work I got a code to generate using script in the HTML file.

--- public/Miscellaneous/printYoutubeTest.js
+++ public/Miscellaneous/printYoutubeTest.js
@@ -33,12 +33,3 @@ if (!window["youtubeIFrame"]) {
 		}
 	});
 }
-
-var qrcode = new QRCode("printqr", {
-    text: "http://jindo.dev.naver.com/collie",
-    width: 128,
-    height: 128,
-    colorDark : "#000000",
-    colorLight : "#ffffff",
-    correctLevel : QRCode.CorrectLevel.H
-});
