c743b25ca342a40f557596ae1ca9fb69029b06fe
Miniland1333 <Miniland1333@users.noreply.github.com>
2020-01-18 11:52:48 -0800

    Beta deployed to Math LibreTexts
    https://math.libretexts.org/Template:Custom/Views/ContentHeader/LibrarySpecific

--- public/Social Share Kit/licenses.js
+++ public/Social Share Kit/licenses.js
@@ -1,36 +1,4 @@
 (function () {
-	function fn() {
-		const cc = getCC();
-		//TODO: Find out WHY this is not needed.
-		/*if (cc) {
-			const thing = document.createElement("li");
-			thing.classList.add("pageInfo");
-			if (cc.label === "gnu") {
-				thing.innerHTML = `<a style="width: max-content; width: -moz-max-content; overflow: initial; font-size:30px" href="${cc.link}"><img style="height: 30px" src="https://awesomefiles.libretexts.org/Social Share Kit/gpl.png" alt="GNU General Public License"/></a>`;
-			}
-			else if (cc.label === "gnudsl") {
-				thing.innerHTML = `<a style="width: max-content; width: -moz-max-content; overflow: initial; font-size:30px" href="${cc.link}"><p style="color: black; font-size: small">GNU Design Science License</p></a>`;
-			}
-			else if (cc.label === "gnufdl") {
-				thing.innerHTML = `<a style="width: max-content; width: -moz-max-content; overflow: initial; font-size:30px" href="${cc.link}"><img style="height: 30px" src="https://awesomefiles.libretexts.org/Social Share Kit/gfdl.png" alt="GNU Free Documentation License"/></a>`;
-			}
-			else if (cc.label === "arr") {
-				thing.innerHTML = `<a style="width: max-content; width: -moz-max-content; overflow: initial; font-size:30px" href="${cc.link}"><img style="height: 30px" src="https://awesomefiles.libretexts.org/Social Share Kit/arr.png" alt="All Rights Reserved"/></a>`;
-			}
-			else {
-				thing.innerHTML = `<a style="width: max-content; width: -moz-max-content; overflow: initial; font-size:30px" href="${cc.link}"><i class='cc ${cc.label}'></i></a>`;
-			}
-			
-			
-			if ($("li.elm-last-modified").length)
-				$("li.elm-last-modified")[0].before(thing);
-			else
-				thing.style.display = "none";
-		}*/
-
-		socialShareConfig();
-	}
-
 	function getCC() {
 		let tags = document.getElementById("pageTagsHolder");
 		if (tags) {
@@ -71,37 +39,8 @@
 		return null; //not found
 	}
 
-	function socialShareConfig() {
-		const isAdmin = document.getElementById("adminHolder").innerText === 'true';
-		if ($(".elm-social-share").length) {
-			let html = '<div class="ssk-group optimize"><div href="" class="ssk ssk-facebook"></div><div href="" class="ssk ssk-twitter"></div><div id="batchPrint"></div>';
-			if (!isAdmin)
-				html += '<a href="https://donorbox.org/libretexts" class="custom-dbox-popup notSS" id="donate"><span>Donate</span></a></div>';
-			$(".elm-social-share")[0].innerHTML = html;
-			SocialShareKit.init();
-			window.DonorBox = { widgetLinkClassName: 'custom-dbox-popup' };
-		}
-
-
-		if (!isAdmin) {
-			const donor = document.createElement("script");
-			donor.type = "text/javascript";
-			donor.src = "https://donorbox.org/install-popup-button.js";
-			if (document.getElementById("donate"))
-				document.getElementById("donate").appendChild(donor);
-		}
-
-		const thing = document.getElementById("pageNumberHolder");
-		if ($("li.elm-page-restriction").length)
-			$("li.elm-page-restriction")[0].after(thing);
-		else
-			thing.style.display = "none";
-	}
-
 	document.getElementById("main_div").innerHTML =
 		`
-            <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
-            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
                 <div class="modal" id="myModal">
 					<div class="modal-content" id="myModal_Slide">
                             <span  id="myModal_Child"></span>
@@ -113,50 +52,53 @@
 
 	function ccDetector() {
 		const cc = getCC();
+        console.log(cc);
 		if (cc) {
             $("#myModal").slideDown(800);
             $('#myModal_Slide').slideDown(800);
 			switch (cc.label) {
 				case "cc-BY":
-					thing_parent.setAttribute("style", "background-color: #ffeb3b;");
+                    thing_parent.setAttribute("style", "background-color: #aed581;");
                     thing.innerHTML = `The content you just copied is ${cc.title} licensed: You can can remix and distribute the work as long as proper attribution is given. Learn more about this license <a href=${cc.link}>here</a> `;
                     break;
 				case "cc-by-sa":
-					thing_parent.setAttribute("style", "background-color: #f44336;");
+                    thing_parent.setAttribute("style", "background-color: #fff176;");
 					thing.innerHTML = `The content you just copied is ${cc.title} licensed: You can remix and distribute the work as long as proper attribution is given and your work also comes with this same license. Learn more about this license <a href=${cc.link}>here</a> `;
 					break;
 				case "cc-by-nc-sa":
-					thing_parent.setAttribute("style", "background-color: #ffeb3b;");
+                    thing_parent.setAttribute("style", "background-color: #fff176;");
 					thing.innerHTML = `The content you just copied is ${cc.title} licensed: You can can remix and distribute the work without profit as long as proper attribution is given and your work also comes with this same license. Learn more about this license <a href=${cc.link}>here</a> `;
 					break;
 				case "cc-by-nc":
-					thing_parent.setAttribute("style", "background-color: #ffeb3b;");
+                    thing_parent.setAttribute("style", "background-color: #fff176;");
 					thing.innerHTML = `The content you just copied is ${cc.title} licensed: <br> You can can remix and distribute the work without profit as long as proper attribution is given. Learn more about this license <a href=${cc.link}>here</a> `;
 					break;
 				case "cc-by-nd":
-					thing_parent.setAttribute("style", "background-color: #f44336;");
+                    thing_parent.setAttribute("style", "background-color: #f44336;");
+                   
 					thing.innerHTML = `The content you just copied is ${cc.title} licensed: You can can share the work if proper attribution is given, but cannot modify it in any way. Learn more about this license <a href=${cc.link}>here</a> `;
 					break;
 				case "cc-by-nc-nd":
-					thing_parent.setAttribute("style", "background-color: #f44336;");
+                    thing_parent.setAttribute("style", "background-color: #f44336;");
 					thing.innerHTML = `The content you just copied is ${cc.title} licensed: You can can share the work without profit if proper attribution is given, but cannot modify it in any way. Learn more about this liscense <a href=${cc.link}>here</a> `;
 					break;
                 case "gnu":
-					thing_parent.setAttribute("style", "background-color: #ffeb3b;");
+                    thing_parent.setAttribute("style", "background-color: #fff176;");
 					thing.innerHTML = `The content you just copied is ${cc.title} licensed: You have the freedom to run, study, share and modify the software. Learn more about this liscense <a href=${cc.link}>here</a>`;
 					break;
 				case "gnudsl":
-					thing_parent.setAttribute("style", "background-color: #f44336;");
+                    thing_parent.setAttribute("style", "background-color: #f44336;");
 					thing.innerHTML = `The content you just copied is ${cc.title} licensed: You have the freedom to run and remix software without profit. Learn more about this license <a href=${cc.link}>here</a> `;
 					break;
 				case "gnufdl":
-					thing_parent.setAttribute("style", "background-color: #f44336;");
+                    thing_parent.setAttribute("style", "background-color: #fff176;");
 					thing.innerHTML = `The content you just copied is ${cc.title} licensed: You have the freedom to run but not remix any software for profit. Learn more about this license <a href=${cc.link}>here</a> `;
 					break;
 				case "arr":
 					thing_parent.setAttribute("style", "background-color: #f44336;");
-					thing.innerHTML = `The content you just copied is ${cc.title} licensed: You are NOT allowed to distribute or remix the content at all.`;
-					break;
+                    thing.innerHTML = `The content you just copied is ${cc.title} licensed: You are NOT allowed to distribute or remix the content at all.`;
+					
+                    break;
 				case "cc-publicdomain":
 					return null;
 					break;
@@ -164,26 +106,19 @@
 
 		}
 	}
-	/*window.onload = function () {
-		span.onclick = function () {
-			modal.style.display = "none";
-		}
-	}*/
-	  window.onclick = function(event) {
+	  window.addEventListener('click',function(event) {
 	  if (event.target == modal) {
           $("#myModal").slideUp(800);
           $('#myModal_Slide').slideUp(800); 
      
       
 	  }
-	} 
+	} );
 
 
-
-	var modal = document.getElementById("myModal");
-	var thing = document.getElementById("myModal_Child");
-	var thing_parent = document.getElementById("myModal_Slide");
+	const modal = document.getElementById("myModal");
+	const thing = document.getElementById("myModal_Child");
+    const thing_parent = document.getElementById("myModal_Slide");
     document.body.appendChild(modal);
 	document.addEventListener("copy", ccDetector);
-	document.addEventListener('DOMContentLoaded', fn);
 })();
