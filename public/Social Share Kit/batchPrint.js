a5c8a8516cf1ba42620cbd49415493f8a599fe6f
Henry Agnew <henryd.agnew@gmail.com>
2020-02-10 15:01:22 -0800

    Compile button mac/safari fix
    Fixed issue due to how safari scopes variables.
    
    Signed-off-by: Henry Agnew <henryd.agnew@gmail.com>

--- public/Social Share Kit/batchPrint.js
+++ public/Social Share Kit/batchPrint.js
@@ -1,18 +1,5 @@
-/*
-Ladda.bind('getLibretext',{callback:(instance)=>{
-		const request = new XMLHttpRequest();
-		request.open("PUT", "https://dynamic.libretexts.org/print/Libretext="+window["TOCName"], true); //async get
-		request.addEventListener("load", receive);
-		request.send(JSON.stringify(window["TOCTable"]));
-
-		function receive(data) {
-			console.log(data);
-		}
-	}});*/
-
 if (!window["batchPrint.js"]) {
 	window["batchPrint.js"] = true;
-	let email = document.getElementById('userEmailHolder').textContent || "";
 	const isAdmin = document.getElementById("adminHolder").innerText === 'true';
 	const isPro = document.getElementById("proHolder").innerText === 'true';
 	const groups = document.getElementById("groupHolder").innerText;
@@ -24,7 +11,6 @@ if (!window["batchPrint.js"]) {
 	let bookstore = tags.find(elem => elem.startsWith('store:'));
 	
 	let fn = () => {
-		HTMLtoJSON();
 		request = new XMLHttpRequest();
 		requestJSON = {
 			root: window.location.href,
@@ -145,188 +131,6 @@ title="BeeLine helps you read on screen more easily by using a color gradient th
 		return JSON;
 	}
 	
-	function thinCC() {
-		alert('This feature is currently under maintenance. 7/9/19');
-		return false;
-		const zip = new JSZip();
-		const textToSave = JSONtoXML();
-		zip.file('imsmanifest.xml', textToSave);
-		zip.generateAsync({type: "blob"})
-			.then(function (blob) {
-				const textToSaveAsURL = window.URL.createObjectURL(blob);
-				const fileNameToSaveAs = `${window["BatchName"]}.imscc`;
-				
-				const downloadLink = document.createElement("a");
-				downloadLink.download = fileNameToSaveAs;
-				downloadLink.innerHTML = "Download File";
-				downloadLink.href = textToSaveAsURL;
-				downloadLink.onclick = this.destroyClickedElement;
-				downloadLink.style.display = "none";
-				document.body.appendChild(downloadLink);
-				
-				downloadLink.click();
-			});
-		
-		/**
-		 * @return {string}
-		 */
-		function JSONtoXML() {
-			let root = window.location.href,
-				batchName = window["BatchName"],
-				title = window["BatchTitle"],
-				subpages = window["BatchTable"];
-			
-			const {org, resources} = createXML(addLinks(subpages, title));
-			
-			function addLinks(object, title) {
-				let result = [];
-				if (Object.keys(object).length) {
-					let resourceArray = [];
-					let readyArray = [];
-					for (let property in object) {
-						if (object.hasOwnProperty(property)) {
-							
-							if (Object.keys(object[property].children).length) { //has children
-								readyArray = readyArray.concat(addLinks(object[property].children, property));
-							}
-							else {
-								resourceArray.push({
-									title: property,
-									url: object[property].link + "?contentOnly"
-								});
-							}
-						}
-					}
-					if (resourceArray.length) //remove empty
-						result.push({title: title, resources: resourceArray});
-					result = result.concat(readyArray);
-					
-				}
-				else { //too shallow
-					result.push({
-						title: title,
-						resources: [{
-							title: title,
-							url: root + "?contentOnly"
-						}],
-					});
-				}
-				return result;
-			}
-			
-			function escapeTitle(unsafe) {
-				return unsafe.replace(/[<>&'"]/g, function (c) {
-					switch (c) {
-						case '<':
-							return '&lt;';
-						case '>':
-							return '&gt;';
-						case '&':
-							return '&amp;';
-						case '\'':
-							return '&apos;';
-						case '"':
-							return '&quot;';
-					}
-				});
-			}
-			
-			function createXML(array) {
-				let org = "";
-				let resources = "";
-				console.log(array);
-				let counter = 1;
-				
-				function getIdentifier() {
-					let result = "T_" + (counter.toString().padStart(6, "0"));
-					counter++;
-					return result;
-				}
-				
-				array.forEach((item) => {
-					if (item.hasOwnProperty("title") && item.hasOwnProperty("resources")) {
-						org += "\n" +
-							`            <item identifier=\"${getIdentifier()}\">\n` +
-							`                <title>${escapeTitle(item.title)}</title>`;
-						item.resources.forEach((resource) => {
-							const identifier = getIdentifier();
-							org += `
-                <item identifier="${identifier}" identifierref="${identifier}_R">
-                    <title>${escapeTitle(resource.title)}</title>
-                </item>`;
-							resources += `
-        <resource identifier="${identifier}_R" type="imswl_xmlv1p1">
-            <file href="${identifier}_F.xml"/>
-        </resource>`;
-							zip.file(`${identifier}_F.xml`,
-								`<?xml version="1.0" encoding="UTF-8"?>
-<webLink xmlns="http://www.imsglobal.org/xsd/imsccv1p1/imswl_v1p1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.imsglobal.org/xsd/imsccv1p1/imswl_v1p1 http://www.imsglobal.org/profile/cc/ccv1p1/ccv1p1_imswl_v1p1.xsd">
-	<title>${escapeTitle(resource.title)}</title>
-	<url href="${resource.url}" target="_iframe"/>
-</webLink>`);
-						});
-						org += "\n" +
-							"            </item>";
-					}
-				});
-				
-				return {org: org, resources: resources};
-			}
-			
-			const top = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-				"<manifest xmlns=\"http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1\" xmlns:lom=\"http://ltsc.ieee.org/xsd/imsccv1p1/LOM/resource\" xmlns:lomimscc=\"http://ltsc.ieee.org/xsd/imsccv1p1/LOM/manifest\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" identifier=\"cctd0015\" xsi:schemaLocation=\"http://www.imsglobal.org/xsd/imslticc_v1p0 http://www.imsglobal.org/xsd/lti/ltiv1p0/imslticc_v1p0.xsd http://www.imsglobal.org/xsd/imslticp_v1p0 http://www.imsglobal.org/xsd/lti/ltiv1p0/imslticp_v1p0.xsd http://www.imsglobal.org/xsd/imslticm_v1p0 http://www.imsglobal.org/xsd/lti/ltiv1p0/imslticm_v1p0.xsd http://www.imsglobal.org/xsd/imsbasiclti_v1p0 http://www.imsglobal.org/xsd/lti/ltiv1p0/imsbasiclti_v1p0p1.xsd\">\n" +
-				"    <metadata>\n" +
-				"        <schema>IMS Common Cartridge</schema>\n" +
-				"        <schemaversion>1.1.0</schemaversion>\n" +
-				"    <lomimscc:lom>\n" +
-				"      <lomimscc:general>\n" +
-				"        <lomimscc:title>\n" +
-				`          <lomimscc:string language=\"en-US\">${escapeTitle(title)}</lomimscc:string>\n` +
-				"        </lomimscc:title>\n" +
-				"      </lomimscc:general>\n" +
-				"    </lomimscc:lom>\n" +
-				"    </metadata>\n" +
-				"    <organizations>\n" +
-				"        <organization identifier=\"T_90000\" structure=\"rooted-hierarchy\">\n" +
-				"        <item identifier=\"T_00000\">";
-			const middle = "\n" +
-				"        </item>\n" +
-				"        </organization>\n" +
-				"    </organizations>\n" +
-				"    <resources>";
-			const end = "\n    </resources>\n" +
-				"</manifest>";
-			
-			return top + org + middle + resources + end;
-		}
-	}
-	
-	function HTMLtoJSON() {
-		// let HTML = $("#batchTreeHolder .wiki-tree");
-		let JSON = {};
-		let URL = window.location.href;
-		const title = document.getElementById("titleHolder").textContent;
-		
-		//get content root
-		URL = URL.split("/").slice(0, 6).join("/");
-		
-		/*		if (HTML[0].children.length) {
-					HTML = HTML[0].children[0].children;
-					let link;
-					
-					for (let i = 0; i < HTML.length; i++) {
-						link = HTML[i].children[0];
-						JSON[link.textContent] = {link: link.href, children: getChildren(HTML[i])};
-					}
-				}*/
-		window["BatchTable"] = JSON;
-		window["BatchName"] = title + URL.length;
-		window["BatchTitle"] = title;
-		// console.log(window["BatchTable"], window["BatchName"]);
-		
-		
-	}
-	
 	function cover(target) {
 		let number = prompt('Number of content pages:');
 		if (number && !isNaN(number)) {
@@ -337,7 +141,8 @@ title="BeeLine helps you read on screen more easily by using a color gradient th
 		}
 	}
 	
-	function batch(target) {
+	// noinspection ES6ConvertVarToLetConst
+	var batch = (target) => {
 		if (!batchAccess) {
 			alert('Authorization Error');
 			return false;
@@ -350,7 +155,6 @@ title="BeeLine helps you read on screen more easily by using a color gradient th
 			request.addEventListener("progress", receive);
 			request.addEventListener("load", download);
 			request.send();
-			//JSON.stringify(requestJSON)
 			const batchButton = document.getElementById("batchButton");
 			batchButton.innerText = 'Request sent...';
 			
@@ -385,7 +189,7 @@ title="BeeLine helps you read on screen more easily by using a color gradient th
 			}
 		}
 		
-	}
+	};
 	
 	document.addEventListener('DOMContentLoaded', fn);
 }
