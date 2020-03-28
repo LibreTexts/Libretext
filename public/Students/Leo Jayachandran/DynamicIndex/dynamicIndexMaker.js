6814916ff98527dacf9481a0e696a43e1970044c
Henry Agnew <henryd.agnew@gmail.com>
2020-03-28 13:44:28 -0700

    Beautified the dynamicIndexMaker code
    Signed-off-by: Henry Agnew <henryd.agnew@gmail.com>

--- public/Students/Leo Jayachandran/DynamicIndex/dynamicIndexMaker.js
+++ public/Students/Leo Jayachandran/DynamicIndex/dynamicIndexMaker.js
@@ -1,15 +1,15 @@
 (function () {
 	processContents();
-	let indexExclusions = ["source", 'lulu' , "@"]; // Case insensitive
-	let indexRequirements =  [""]; // Case sensisitve  if ends with @, that tag will be trimmed, else it will be left as it(Can be entered here or in currentScript.dataset.filter, currentScript.dataset.filter takes priority)
+	let indexExclusions = ["source", 'lulu', "@"]; // Case insensitive
+	let indexRequirements = [""]; // Case sensisitve  if ends with @, that tag will be trimmed, else it will be left as it(Can be entered here or in currentScript.dataset.filter, currentScript.dataset.filter takes priority)
 	
 	
 	//main function
 	async function processContents() {
-        $(document.currentScript).after(`<div id="indexDiv"><p id="indexLetterList"></p><div id="indexTable"></div></div>`);
-        if (typeof(document.currentScript.dataset) !== "undefined" && typeof(document.currentScript.dataset.filter) !== "undefined") {
-            indexRequirements = ((document.currentScript.dataset.filter.startsWith("[")) ? JSON.parse(document.currentScript.dataset.filter) : [document.currentScript.dataset.filter]);
-        }
+		$(document.currentScript).after(`<div id="indexDiv"><p id="indexLetterList"></p><div id="indexTable"></div></div>`);
+		if (typeof (document.currentScript.dataset) !== "undefined" && typeof (document.currentScript.dataset.filter) !== "undefined") {
+			indexRequirements = ((document.currentScript.dataset.filter.startsWith("[")) ? JSON.parse(document.currentScript.dataset.filter) : [document.currentScript.dataset.filter]);
+		}
 		let subdomain = window.location.origin.split('/')[2].split('.')[0];
 		let coverPageInfo;
 		
@@ -112,7 +112,7 @@
 		let firstLoopCheck = 0;
 		while (termCutString.length > 0) { //loop through the different terms
 			//Find the title of the term
-            let termName = termCutString.substring(termCutString.search(preTermName) + preTermName.length, termCutString.search(postTermName));
+			let termName = termCutString.substring(termCutString.search(preTermName) + preTermName.length, termCutString.search(postTermName));
 			let newTerm = {
 				"name": termName,
 				"pages": [
@@ -146,36 +146,37 @@
 				cutString = cutString.slice(termEnd + termEndString.length);
 				newTerm.pages.push(newPage);
 				
-            }
-            
+			}
+			
 			//update termcutstring
-            termCutString = termCutString.slice(postPagelistPosition + postPagelist.length);
-            
-            let canPush = true;
-            //Filter term
-            
-            for (let e = 0; e < indexRequirements.length; e++) { // Inclusions
-                if (canPush && newTerm.name.includes(indexRequirements[e])) { // if requirement is present, trim and continue check, if term is absent, 
-                    if (indexRequirements[e].endsWith("@")) { // Trim the term name if filter ends with @
-                        newTerm.name = newTerm.name.replace(indexRequirements[e], "").trim();
-                    }
-                } else { // stop checking
-                    canPush = false;
-                    break;
-                }
-            }
-            
-            for (let e = 0 ; e < indexExclusions.length; e++) { // Exclusions
-                if (canPush && newTerm.name.toLowerCase().includes(indexExclusions[e])) { //Stop check if fails
-                    canPush = false;
-                    break;
-                }
-            }
-            //Push new term
-            if (canPush) {
-                pageList.taggedTerms.push(newTerm);
-            }
-            
+			termCutString = termCutString.slice(postPagelistPosition + postPagelist.length);
+			
+			let canPush = true;
+			//Filter term
+			
+			for (let e = 0; e < indexRequirements.length; e++) { // Inclusions
+				if (canPush && newTerm.name.includes(indexRequirements[e])) { // if requirement is present, trim and continue check, if term is absent,
+					if (indexRequirements[e].endsWith("@")) { // Trim the term name if filter ends with @
+						newTerm.name = newTerm.name.replace(indexRequirements[e], "").trim();
+					}
+				}
+				else { // stop checking
+					canPush = false;
+					break;
+				}
+			}
+			
+			for (let e = 0; e < indexExclusions.length; e++) { // Exclusions
+				if (canPush && newTerm.name.toLowerCase().includes(indexExclusions[e])) { //Stop check if fails
+					canPush = false;
+					break;
+				}
+			}
+			//Push new term
+			if (canPush) {
+				pageList.taggedTerms.push(newTerm);
+			}
+			
 		}
 		return pageList;
 	}
@@ -192,8 +193,8 @@
 		}
 		return newList;
 	}
-
-
+	
+	
 	function sortPage(pageList/*Array with Pages*/) {
 		let newList = pageList.slice();
 		newList.sort(function (a, b) {
@@ -245,8 +246,8 @@
 			for (let u = termPos; u < pageList.taggedTerms.length; u++) {
 				
 				if (pageList.taggedTerms[u].name.toUpperCase().startsWith(alphabet[i])) { // Check if term is in letter
-                    
-                    indexLetterTerm["terms"].push(pageList.taggedTerms[u]);
+					
+					indexLetterTerm["terms"].push(pageList.taggedTerms[u]);
 					termPos++;
 				}
 				else {
@@ -278,12 +279,12 @@
 				let $termText = $(`<p>${alphabetisedIndex[i].terms[u].name}</p>`);
 				let $pagesText = $(`<div class = "pagesTextDiv"></div>`);
 				for (let j = 0; j < alphabetisedIndex[i].terms[u].pages.length; j++) {
-                    let pageToAdd = $(`<a></a>`).html(alphabetisedIndex[i].terms[u].pages[j].pageName).attr({
-                        "title": alphabetisedIndex[i].terms[u].pages[j].pageName,
-                        "href": alphabetisedIndex[i].terms[u].pages[j].pageLink,
-                        "class" : "indexPages"
-                    });
-
+					let pageToAdd = $(`<a></a>`).html(alphabetisedIndex[i].terms[u].pages[j].pageName).attr({
+						"title": alphabetisedIndex[i].terms[u].pages[j].pageName,
+						"href": alphabetisedIndex[i].terms[u].pages[j].pageLink,
+						"class": "indexPages"
+					});
+					
 					$pagesText.append(pageToAdd, `&#10; <br/>`);
 				}
 				$termDiv.append($termText, $pagesText);
