9693e8f146e1955db81e6b00b817cba75bcd31a7
Aryan <aryanrsuri@gmail.com>
2020-08-04 10:53:01 -0700

    New updates; removed html

--- public/Sidebar/sidebar.js
+++ public/Sidebar/sidebar.js
@@ -1,50 +1,63 @@
-var entire = document.getElementById("f");
+window.addEventListener("load", fbButtons);
 
-entire.innerHTML=`
+function fbButtons() {
 
-<button onclick="frontpage()" id="f_button"><i id="right" class="fa fa-long-arrow-right "></i></button>
-<div id="f_title" ></div>
-<button onclick="backpage()" id="b_button"><i id="left" class="fa fa-long-arrow-left "></i></button>
-<div id="b_title"></div>
+   let first_div = document.createElement("div");
+   document.body.appendChild(first_div);
+   first_div.id = "f";
 
-`;
-
-let f_b = document.getElementById("f_button");
-let b_b = document.getElementById("b_button");
-document.body.appendChild(f_b);
-document.body.appendChild(b_b);
+   console.log("Function here");
 
 
-$(document).ready(function() {
-    $('#f_title').text($("a.mt-icon-next-article span").first().text());
-    $('#b_title').text($("a.mt-icon-previous-article span").first().text());
-});
+   $("#f").html(`
+
+    
+    <button onclick="frontpage()" id="f_button"><i id="right" class="fa fa-long-arrow-right "></i></button>
+    <div class="button_title"  id="f_title" ></div>
+    <button onclick="backpage()"  id="b_button"><i id="left" class="fa fa-long-arrow-left "></i></button>
+    <div class="button_title"  id="b_title"></div>`);
 
 
-$("#f_button").hover( function(){
-   $('#f_title').fadeIn(200); 
-   }, function(){
-   $('#f_title').fadeOut(200);
+   let f_b = document.getElementById("f_button");
+   let b_b = document.getElementById("b_button");
 
-});
+   first_div.append(f_b);
+   first_div.append(b_b);
 
-$("#b_button").hover( function(){
-   $('#b_title').fadeIn(200); 
-   }, function(){
-   $('#b_title').fadeOut(200);
 
-});
+   $(document).ready(function () {
+      $('#f_title').text($("a.mt-icon-next-article span").first().text());
+      $('#b_title').text($("a.mt-icon-previous-article span").first().text());
+   });
 
-function frontpage(){
-   var x = $('.mt-icon-next-article').closest('a').attr('href');
-        console.log(x);
-  
-  this.location.href = x;
-}
-function backpage(){
-  
-  var x = $('.mt-icon-previous-article').closest('a').attr('href');
-        console.log(x);
-  
-  this.location.href = x;
+
+   $("#f_button").hover(function () {
+      $('#f_title').fadeIn(200);
+   }, function () {
+      $('#f_title').fadeOut(200);
+
+   });
+
+   $("#b_button").hover(function () {
+      $('#b_title').fadeIn(200);
+   }, function () {
+      $('#b_title').fadeOut(200);
+
+   });
+
+
+   function frontpage() {
+      var x = $('.mt-icon-next-article').closest('a').attr('href');
+      console.log(x);
+
+      this.location.href = x;
+   }
+   function backpage() {
+
+      var x = $('.mt-icon-previous-article').closest('a').attr('href');
+      console.log(x);
+
+      this.location.href = x;
+   }
+
 }
