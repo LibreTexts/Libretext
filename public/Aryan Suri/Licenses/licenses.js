47ec1cb3663028570c3efe0eb76d45b460f9eccc
Miniland1333 <Miniland1333@users.noreply.github.com>
2020-08-25 19:40:25 -0700

    Remove old SocialShareKit code

--- public/Aryan Suri/Licenses/licenses.js
+++ public/Aryan Suri/Licenses/licenses.js
@@ -226,8 +226,10 @@ function getCC() {
 			let html = '<div class="ssk-group optimize"><div href="" class="ssk ssk-facebook"></div><div href="" class="ssk ssk-twitter"></div><div id="batchPrint"></div>';
 			if (!isAdmin)
 				html += '<a href="https://donorbox.org/libretexts" class="custom-dbox-popup notSS" id="donate"><span>Donate</span></a></div>';
+			else
+			        html +='</div>';
 			$(".elm-social-share")[0].innerHTML = html;
-			SocialShareKit.init();
+			//SocialShareKit.init();
 			window.DonorBox = { widgetLinkClassName: 'custom-dbox-popup' };
 		}
 
