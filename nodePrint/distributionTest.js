8322804d380bd19a637f925db0a3aca01ae37828
Henry Agnew <henryd.agnew@gmail.com>
2020-11-29 15:33:49 -0800

    Added api/bounce for file CORs
    Also removed Optimist dependency
    
    Signed-off-by: Henry Agnew <henryd.agnew@gmail.com>

--- nodePrint/distributionTest.js
+++ nodePrint/distributionTest.js
@@ -1,17 +1,17 @@
 const maxAge = 60;
 const results = {};
 for (let i = 0; i <= 1000000; i++) {
-	for (let j = 0; j <= maxAge; j+=7) {
-		let randomCache = Math.random() * 2; //0 to 2
-		randomCache = j > (randomCache + 0.2) * maxAge;
-		if (randomCache) {
-			if (!results[j])
-				results[j] = 0;
-			results[j]++;
-			break;
-		}
-	}
+    for (let j = 0; j <= maxAge; j += 7) {
+        let randomCache = Math.random() * 2; //0 to 2
+        randomCache = j > (randomCache + 0.2) * maxAge;
+        if (randomCache) {
+            if (!results[j])
+                results[j] = 0;
+            results[j]++;
+            break;
+        }
+    }
 }
 // console.log(results);
 for (key in results)
-	console.log(`${key}\t${results[key]}`);
+    console.log(`${key}\t${results[key]}`);
