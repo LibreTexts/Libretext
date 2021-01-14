1c6a5788c906ed71ef3c3f8e811cc9d755eb7cf8
Aryan Suri <arsuri@ucdavis.edu>
2021-01-13 20:03:02 -0800

    attribution generator

--- public/Aryan Suri/OERBuilder/oerattr.ts
+++ public/Aryan Suri/OERBuilder/oerattr.ts
@@ -1,5 +1,6 @@
 function oerbuilder() {
     const OER = document.createElement("div");
+    let OUTPUT = ``;
     OER.id = "OERDIV";
     document.body.append(OER);
 
@@ -7,22 +8,46 @@ function oerbuilder() {
         `
         <div onclick="hideoer()" id="OERMODAL">
             <div id="OERCONTENT">
+                <h3> Attribution Builder</h3>
                 <input placeholder="Title" id="oertitle">
                 <input placeholder="Title URL" id="oertitleurl">
                 <input placeholder="Author" id="oerauthor">
                 <input placeholder="Author URL" id="oerauthorurl">
-                <button onclick="oer()" id="oersubmit">create attribution</button>
-                <div id="OEROUTPUT"></div>
+                <select id="oerlicense"><option value="CC-BY">CC-BY</option> <option value="CC-BY-NA">CC-BY-NA</option></select>
+
+                <div id="OEROUTPUT"><a id="OERTITLE"></a><a id="OERAUTHOR"></a><a id="OERORG"></a><a id="OERLICENSE"></a></div>
             </div>
         </div>
  `
     )
-
-    $("#oersubmit").on("click", ()=> {
-        console.log("works");
+    $("#oertitle").on("change", ()=>{
+        let TITLE  = document.querySelector("#oertitle").value;
+        document.getElementById("OERTITLE")!.innerText = `"${TITLE}"`;
     });
 
-}
+    $("#oertitleurl").on("change", ()=>{
+        let TITLEURL  = document.querySelector("#oertitleurl").value;
+        let a = document.getElementById("OERTITLE")!;
+        a.href = `${TITLEURL}`;
+    });
+
+    $("#oerauthor").on("change", ()=>{
+        let AUTHOR  = document.querySelector("#oerauthor").value;
+        document.getElementById("OERAUTHOR")!.innerText = `by ${AUTHOR}, `;
+    });
+
+    $("#oerauthorurl").on("change", ()=>{
+        let AUTHORURL  = document.querySelector("#oerauthorurl").value;
+        let aa = document.getElementById("OERAUTHOR")!;
+        aa.href = `${AUTHORURL}`;
+    });
+
+    $("#oerlicense").on("change", ()=>{
+        let LICENSE  = document.querySelector("#oerlicense").value;
+        document.getElementById("OERLICENSE")!.innerText = `is licensed under ${LICENSE} `;
+    });
+
+
 
 
 function hideoer() {
