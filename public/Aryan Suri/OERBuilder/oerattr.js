fed23b399c4c77ab087b0525aaaa887a8f40baf4
Aryan Suri <arsuri@ucdavis.edu>
2021-01-14 14:51:26 -0800

    OER PATCH ONE

--- public/Aryan Suri/OERBuilder/oerattr.js
+++ public/Aryan Suri/OERBuilder/oerattr.js
@@ -1,11 +1,10 @@
+"use strict";
 function oerbuilder() {
     const OER = document.createElement("div");
     let OUTPUT = ``;
     OER.id = "OERDIV";
     document.body.append(OER);
-
-    $(OER).html(
-        `
+    $(OER).html(`
         <div onclick="hideoer()" id="OERMODAL">
             <div id="OERCONTENT">
                 <h3> Attribution Builder</h3>
@@ -31,56 +30,44 @@ function oerbuilder() {
                 <div id="OEROUTPUT"><a id="OERTITLE"></a><a id="OERAUTHOR"></a><a id="OERORG"></a><a id="OERLICENSE"></a></div>
             </div>
         </div>
- `
-    )
+ `);
     $("#oertitle").on("change", () => {
         let TITLE = document.querySelector("#oertitle").value;
-        document.getElementById("OERTITLE")!.innerText = `"${TITLE}" `;
+        document.getElementById("OERTITLE").innerText = `"${TITLE}" `;
     });
-
     $("#oertitleurl").on("change", () => {
         let TITLEURL = document.querySelector("#oertitleurl").value;
-        if (!TITLEURL.match(/^[a-zA-Z]+:\/\//))
-        {
+        if (!TITLEURL.match(/^[a-zA-Z]+:\/\//)) {
             TITLEURL = 'http://' + TITLEURL;
         }
-        let a = document.getElementById("OERTITLE")!;
+        let a = document.getElementById("OERTITLE");
         a.href = `${TITLEURL}`;
     });
-
     $("#oerauthor").on("change", () => {
         let AUTHOR = document.querySelector("#oerauthor").value;
-        document.getElementById("OERAUTHOR")!.innerText = `by ${AUTHOR} `;
+        document.getElementById("OERAUTHOR").innerText = `by ${AUTHOR} `;
     });
-
     $("#oerauthorurl").on("change", () => {
         let AUTHORURL = document.querySelector("#oerauthorurl").value;
-        let aa = document.getElementById("OERAUTHOR")!;
+        let aa = document.getElementById("OERAUTHOR");
         aa.href = `${AUTHORURL}`;
     });
-
     $("#oerorg").on("change", () => {
         let ORG = document.querySelector("#oerorg").value;
-        document.getElementById("OERORG")!.innerText = `, ${ORG} `;
+        document.getElementById("OERORG").innerText = `, ${ORG} `;
     });
-
     $("#oerorgurl").on("change", () => {
         let ORGURL = document.querySelector("#oerorgurl").value;
-        let aaa = document.getElementById("OERORG")!;
+        let aaa = document.getElementById("OERORG");
         aaa.href = `${ORGURL}`;
     });
-
-
     $("#oerlicense").on("change", () => {
         let LICENSE = document.querySelector("#oerlicense").value;
-        document.getElementById("OERLICENSE")!.innerText = `is licensed under ${LICENSE}. `;
+        document.getElementById("OERLICENSE").innerText = `is licensed under ${LICENSE}. `;
     });
-
 }
-
-
 function hideoer() {
-    if (!$(event!.target!).closest('#OERCONTENT').length && !$(event!.target!).is('#OERCONTENT')) {
+    if (!$(event.target).closest('#OERCONTENT').length && !$(event.target).is('#OERCONTENT')) {
         $("#OERDIV").remove();
     }
 }
