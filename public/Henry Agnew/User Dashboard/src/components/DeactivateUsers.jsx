20892abfb96127cde440853d1bf5887939b38ffe
Henry Agnew <henryd.agnew@gmail.com>
2021-01-22 22:56:29 -0800

    Almost ready to run.
    Signed-off-by: Henry Agnew <henryd.agnew@gmail.com>

--- public/Henry Agnew/User Dashboard/src/components/DeactivateUsers.jsx
+++ public/Henry Agnew/User Dashboard/src/components/DeactivateUsers.jsx
@@ -5,80 +5,106 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Table from "@material-ui/core/Table";
 import TableHead from '@material-ui/core/TableHead';
-import TableBody from "@material-ui/core/TableBody";
 import Button from "@material-ui/core/Button";
+import * as PropTypes from "prop-types";
+import {LinearProgress} from "@material-ui/core";
+import {FixedSizeList as List} from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 
 export default function DeactivateUsers(props) {
     const [users, setUsers] = React.useState([]);
+    const [total, setTotal] = React.useState();
     const [subdomain] = LibreTexts.parseURL();
+    
     useEffect(() => {
         //fetch data from previous batches
-        async function fetchData() {
+        async function fetchData() {//
             let response = await fetch(`https://${subdomain}.libretexts.org/@api/deki/users?verbose=false&limit=all&dream.out.format=json`);
             response = await response.json();
             response = response.user;
-            let never = [];
             response = response.filter(item => item['@id'] > 2 && item.status === 'active')
             response.forEach(item => {
                 if (!item['date.lastlogin']) {
-                    never.push(item.username)
                     item['date.lastlogin'] = item['date.created']
                 }
-                item.id =item['@id'];
-                item.deleted = false;
+                item.id = item['@id'];
             });
-            console.log(never);
-            response = response.filter(item => new Date(item['date.lastlogin']) < new Date('1/1/20'))
+            response = response.filter(item => new Date() - new Date(item['date.lastlogin']) > 3.154e+10) //older than 1 year
             response = response.sort((a, b) => new Date(a['date.lastlogin']) - new Date(b['date.lastlogin']));
-            response = response.slice(0, 1000); //TODO replace with pagination
+            setTotal(response.length);
             setUsers(response);
         }
         
         fetchData();
     }, [])
     
-    function UserRow(prop) {
-        const user = prop.user;
-        return <TableRow key={user.id} style={{textDecoration: user.deleted ? 'line-through' : undefined, color:'red'}}>
-            <TableCell style={{textAlign: 'center'}}>
-                {user.username}
-            </TableCell>
-            <TableCell>
-                {user['date.lastlogin']}
-            </TableCell>
-        </TableRow>
-    }
     
     async function purgeUsers() {
-        for (let user of users) {
-            const id = user['@id'];
-/*            await fetch(`https://${subdomain}.libretexts.org/@api/deki/users/${id}`, {
+        const fullArray = JSON.parse(JSON.stringify(users));
+        const workingArray = JSON.parse(JSON.stringify(users));
+        for (const currentUser of fullArray) {
+            const id = currentUser['@id'];
+ /*           await fetch(`https://${subdomain}.libretexts.org/@api/deki/users/${id}`, {
                 method: 'PUT',
                 headers: {'content-type': 'application/xml; charset=utf-8'},
                 body: `<user><status>inactive</status></user>`
             })*/
-            user.deleted = true;
-            setUsers(users);
-            console.log(`Deleted ${id}`)
-            await LibreTexts.sleep(2000);
-            // return;
+            workingArray.shift()
+            setUsers([...workingArray]);
+            console.log(`Deactivated ${id}`)
+            await LibreTexts.sleep(100);
+        }
+    }
+    
+    function getProgress() {
+        if (!users?.length)
+            return <div>Retrieving users<br/><LinearProgress color="primary"/></div>;
+        else {
+            const percent = (total - users.length) / total * 100;
+            return <div><LinearProgress color="secondary" variant="determinate"
+                                                         value={percent}/>{Math.trunc(percent)}%</div>
         }
     }
     
     return (
         <div id="BatchMonitor">
             <div className="topPanel">
-                This tool is used to deactivate all counts that have been unused for over a year.
-                <Button onClick={purgeUsers} color="secondary"
-                        variant="contained">PURGE {users.length} accounts</Button>
+                <div>This tool is used to deactivate all counts that have been unused for over a year. Accounts will be
+                     moved from active to inactive status, but will not be deleted.
+                    <Button onClick={purgeUsers} color="secondary"
+                            variant="contained">DEACTIVATE {users.length} accounts</Button></div>
+                {getProgress()}
                 <TableContainer size="small" component={Paper}>
                     <Table size="small" style={{display: 'inline-table'}}>
                         <TableHead><TableRow>
                             <TableCell>Username ({users.length})</TableCell>
                             <TableCell>Last Login</TableCell>
                         </TableRow></TableHead>
-                        <TableBody>{users.map(user=><UserRow key={user.id} user={{...user}}/>)}</TableBody>
+                        <AutoSizer disableHeight={true}>
+                            {({height, width}) => (
+                                <List
+                                    className="List"
+                                    height={500}
+                                    itemCount={users.length}
+                                    itemSize={40}
+                                    width={width}
+                                >
+                                    {({index, style}) => {
+                                        let user = users[index];
+                                        return <tr key={user.id}
+                                                   style={{...style, display: 'flex'}}>
+                                            <div style={{flex: 1, textAlign: 'center'}}>
+                                                {user.username}
+                                            </div>
+                                            <div style={{flex: 1}}>
+                                                {user['date.lastlogin']}
+                                            </div>
+                                        </tr>
+                                    }}
+                                </List>
+                            )}
+                        </AutoSizer>
                     </Table>
                 </TableContainer>
             </div>
