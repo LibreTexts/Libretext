15c8d3b0af5b3a9273862ee868648e08d7c4cd7c
Henry Agnew <henryd.agnew@gmail.com>
2021-07-04 16:33:57 -0700

    Initial React Sidebar
    Rewrite of Sidebar for better accessibility
    
    Signed-off-by: Henry Agnew <henryd.agnew@gmail.com>

---
+++ public/Binh Nguyen/ReactSidebar/src/pages/index.js
@@ -0,0 +1,42 @@
+/*
+This code imports external libraries so that React can use them!
+These pieces are code are then bundled into your application during the compilation process.
+Always place your imports at the top of files!
+*/
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import SubComponent from "../components/SubComponent.jsx";
+
+
+/*
+This code injects your React code into the webpage.
+*/
+const target = document.createElement("div");
+// noinspection JSValidateTypes
+target.id = Math.random() * 100;
+// noinspection XHTMLIncompatabilitiesJS
+document.currentScript.parentNode.insertBefore(target, document.currentScript);
+
+
+/*
+This is your React Hook.
+Your top-level logic should go here, but other parts should be handled by sub-components.
+*/
+function HelloWorld(props) {
+	const [buttonText, setButtonText] = useState("Click me, please");
+	const [numClicks, setNumClicks] = useState(0);
+	
+	return (<React.Fragment>
+			<button onClick={() => {
+				setNumClicks(numClicks + 1);
+				setButtonText(`Thanks, I've been clicked ${numClicks + 1} times!`)
+			}}>
+				{buttonText}
+			</button>
+			<SubComponent number={numClicks}/>
+		</React.Fragment>
+	);
+}
+
+
+ReactDOM.render(<HelloWorld/>, target);
