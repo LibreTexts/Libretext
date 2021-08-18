68d8f068f65e789a81ff550c7f091938aa4c0709
Aryan Suri <aryanrsuri@gmail.com>
2021-08-17 17:36:02 -0700

    v2.2.0

--- public/Aryan Suri/RefManager/RefManager.ts
+++ public/Aryan Suri/RefManager/RefManager.ts
@@ -1,21 +1,44 @@
+// @author: Aryan Suri
 window.addEventListener("load", async () => {
     buildManager();
     await processReference();
 });
 
-// Table of Contents:
-// buildManager, updateManager, storeReference, deleteReference, processReference --> replaceReferenceID
-
-
 function buildManager(){
-    const managerArea: HTMLDivElement = document.createElement('div');
-    const referenceArea = document.createElement('ul');
+    const referenceModalButton = document.createElement("button");
+    const referenceModal = document.createElement("div");
+    referenceModalButton.innerHTML = `Citation Manager`;
+    referenceModalButton.id = "referenceModalBtn";
+    referenceModal.innerHTML = `
+        <div id="referenceModal">
+            <div id="referenceModalContent">
+            <h3> Citation Manager</h3>
+            <div id="referenceInput">
+                <input type="text" id="referenceInput-Text" value=""> 
+                <button onclick="storeReference(document.getElementById('referenceInput-Text').value)">Cite</button>
+            </div>
+            <ul id="referenceDisplay"></ul>
+            </div>
+        </div>
+    `
 
-    managerArea.innerHTML =  `<input type="text" id="referenceInput-Text" value=""> <button onclick="storeReference(document.getElementById('referenceInput-Text').value)">Cite</button>`
-    referenceArea.id='referenceDisplay';
-    managerArea.id = 'referenceInput';
-    document.getElementById("elm-header")!.appendChild(managerArea);
-    document.getElementById("elm-header")!.appendChild(referenceArea);
+    document.body.append(referenceModal);
+    document.getElementsByClassName("elm-social-share")[0].appendChild(referenceModalButton);
+
+    const modal = document.getElementById("referenceModal")!;
+    const btn = document.getElementById("referenceModalBtn")!;
+    //const span = document.getElementById("referenceModalClose")!;
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+    // span.onclick = function() {
+    //     modal.style.display = "none";
+    // }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
     updateManager(true)
 }
 
@@ -24,9 +47,19 @@ function updateManager(refresh: boolean, ref: any = JSON.parse(<string>localStor
         try {
             for (let key in ref) {
                 let item: HTMLLIElement = document!.createElement("li");
+                item.innerText = `Citation: ${ref[key].citation}         ${ref[key].id}`
+                let i = ref[key].id
                 // @ts-ignore
-                item.onclick = deleteReference;
-                item.innerText = "ID# " + ref[key].id + "\n"+ "Citation:  " + ref[key].citation;
+                item.onclick =()=>{  const el = document.createElement('textarea');
+                    el.value = `\\#${ref[key].id}#\\`;
+                    el.setAttribute('readonly', '');
+                    el.style.position = 'absolute';
+                    el.style.left = '-9999px';
+                    document.body.appendChild(el);
+                    el.select();
+                    document.execCommand('copy');
+                    document.body.removeChild(el);
+                    alert("Citation ID copied")};
                 document.getElementById('referenceDisplay')!.appendChild(item);
             }
         }
@@ -35,10 +68,18 @@ function updateManager(refresh: boolean, ref: any = JSON.parse(<string>localStor
         }
     }
     else {
-        let item = document.createElement("li");
-        // @ts-ignore
-        item.onclick = deleteReference;
-        item.innerText = "ID# " + ref.id + "\n"+ "Citation:  " + ref.citation;
+        let item: HTMLLIElement = document!.createElement("li");
+        item.innerText = `Citation: ${ref.citation}         /#${ref.id}#/`
+        item.onclick =()=>{const el = document.createElement('textarea');
+            el.value = `/#${ref.id}#/`;
+            el.setAttribute('readonly', '');
+            el.style.position = 'absolute';
+            el.style.left = '-9999px';
+            document.body.appendChild(el);
+            el.select();
+            document.execCommand('copy');
+            document.body.removeChild(el);
+            alert("Citation ID copied")};
         document.getElementById('referenceDisplay')!.appendChild(item);
     }
 }
@@ -73,20 +114,33 @@ async function storeReference(data: any){
 
     let userRefJSON: any;
     try {
-        userRefJSON = await LibreTexts.authenticatedFetch(coverPage,`files/=references.json`,null);
+        userRefJSON = await LibreTexts.authenticatedFetch(null,`files/=references.json`,null);
         userRefJSON = await userRefJSON.json();
     } catch(e) {
         userRefJSON = {};
     }
     userRefJSON[parseReference[0].id] = referenceGlobal;
-    await LibreTexts.authenticatedFetch(coverPage,`files/=references.json`,null, {
+    await LibreTexts.authenticatedFetch(null,`files/=references.json`,null, {
         method:"PUT",
         body:(JSON.stringify(userRefJSON))
     });
 
     updateManager(false, referenceLocal);
 }
-
+function copyReference(str: string){
+    console.log("fnc called")
+    console.log(str);
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    return;
+}
 function deleteReference(this: HTMLElement) {
     const reg = new RegExp(".*ID#\\s*([^\\n\\r]*)");
     const ID = this.innerText.match(reg)![1]
@@ -102,7 +156,7 @@ async function processReference(){
     const reg = new RegExp(/(?:\\#)([\s\S]*?)(?:#\\)/gm);
     let referenceJSON: any;
     try {
-        referenceJSON = await LibreTexts.authenticatedFetch(coverPage,`files/=references.json`,null);
+        referenceJSON = await LibreTexts.authenticatedFetch(null,`files/=references.json`,null);
         referenceJSON = await referenceJSON.json();
     } catch(e) {
         console.log(e)
@@ -116,10 +170,29 @@ async function processReference(){
         return "Citation not Found";
     }
     let procReference: string = pageContent.replace(reg, (match, offset, string) => {
-        console.log(`match found: ${match}`);
         const trimmedMatch = match.substring(2, match.length - 2).trim();
         return replaceReferenceID(referenceJSON, trimmedMatch);
     });
 
     document.getElementById("pageText")!.innerHTML = procReference;
+}
+
+async function processBibliography() {
+    const Cite = CitRequire('citation-js');
+    // data is going to be from the references.json file
+    let referenceJSON;
+    try {
+        referenceJSON = await LibreTexts.authenticatedFetch(null,`files/=references.json`,null);
+        referenceJSON = await referenceJSON.json();
+    } catch(e) {
+        console.log(e)
+    }
+    console.log(referenceJSON)
+    // do a for key in ref (iterate thru json)
+    // for each key, take ref[key].data
+    // use that for data
+    const Data = new Cite(data);
+    const reference = Data.format('data');
+    const managerArea: HTMLDivElement = document.createElement('div');
+    const referenceArea = document.createElement('ul');
 }
