6e9964d81efb1c257b0d2bf695dad1735f36bfa4
Ethan Turner <ethanaturner13@gmail.com>
2021-11-17 16:36:04 -0800

    Upgrade LibreText nav buttons for intra-Text only

--- public/NavButtons/libreNavButtons.js
+++ public/NavButtons/libreNavButtons.js
@@ -6,13 +6,34 @@ function libreNavButtons() {
 
 	if ($('#pageTagsHolder').text().includes('"article:topic"') || $('#pageTagsHolder').text().includes('"article:topic-guide"')) {
 
+		let displayPrev = true;
+		let displayNext = true;
+
 		let prevArticle = $("a.mt-icon-previous-article").first();
 		let nextArticle = $("a.mt-icon-next-article").first();
 
 		let prevPage = prevArticle.attr('href');
-		let prevPageTitle = prevArticle.attr('title');
-
 		let nextPage = nextArticle.attr('href');
+
+		// Attempt to disable navigation button(s) if they lead outside of the current LibreText
+		try {
+			let breadCrumbs = $("ol.mt-breadcrumbs").first();
+			let breadCrumbsList = breadCrumbs.find('li');
+			if (breadCrumbsList.length >= 4) {
+				let bookCrumb = breadCrumbsList.get(3);
+				if (bookCrumb) {
+					let bookHref = $(bookCrumb).find('a').first().attr('href');
+					if (bookHref) {
+						displayPrev = prevPage.includes(bookHref);
+						displayNext = nextPage.includes(bookHref);
+					}
+				}
+			}
+		} catch (e) {
+			console.log("[ERROR] LibreNavButtons: ", e.message);
+		}
+
+		let prevPageTitle = prevArticle.attr('title');
 		let nextPageTitle = nextArticle.attr('title');
 
 		let backButton = document.createElement('a');
@@ -22,51 +43,56 @@ function libreNavButtons() {
 		let nextTitle = document.createElement('div');
 		let nextTitleText = document.createElement('span');
 
-		backButton.href = prevPage;
-		backButton.setAttribute('id', 'backButton');
-		backButton.className = 'libreNavBtn';
-		backButton.setAttribute('aria-label', prevPageTitle);
+		if (displayPrev) {
+			backButton.href = prevPage;
+			backButton.setAttribute('id', 'backButton');
+			backButton.className = 'libreNavBtn';
+			backButton.setAttribute('aria-label', prevPageTitle);
 
-		backTitle.setAttribute('id', 'backTitle');
-		backTitle.className = 'libreNavTitle';
+			backTitle.setAttribute('id', 'backTitle');
+			backTitle.className = 'libreNavTitle';
 
-		backTitleText.setAttribute('id', 'backTitleText');
-		backTitleText.className = 'libreNavText';
-		backTitleText.innerText = prevPageTitle;
+			backTitleText.setAttribute('id', 'backTitleText');
+			backTitleText.className = 'libreNavText';
+			backTitleText.innerText = prevPageTitle;
 
-		nextButton.href = nextPage;
-		nextButton.setAttribute('id', 'nextButton');
-		nextButton.className = 'libreNavBtn';
-		nextButton.setAttribute('aria-label', nextPageTitle);
+			$(backButton).html(`<i id='backButtonIcon' class='libreNavIcon fa fa-arrow-left'></i>`);
 
-		nextTitle.setAttribute('id', 'nextTitle');
-		nextTitle.className = 'libreNavTitle';
+			$(backTitle).append(backTitleText);
 
-		nextTitleText.setAttribute('id', 'nextTitleText');
-		nextTitleText.className = 'libreNavText';
-		nextTitleText.innerText = nextPageTitle;
+			$(backButton).hover(function () {
+				$(backTitle).css('display', 'flex').fadeIn(200);
+			}, function () {
+				$(backTitle).css('display', 'none').fadeOut(200);
+			});
+			document.body.append(backButton);
+			document.body.append(backTitle);
+		}
 
-		$(backButton).html(`<i id='backButtonIcon' class='libreNavIcon fa fa-arrow-left'></i>`);
-		$(nextButton).html(`<i id='nextButtonIcon' class='libreNavIcon fa fa-arrow-right'></i>`);
+		if (displayNext) {
+			nextButton.href = nextPage;
+			nextButton.setAttribute('id', 'nextButton');
+			nextButton.className = 'libreNavBtn';
+			nextButton.setAttribute('aria-label', nextPageTitle);
 
-		$(backTitle).append(backTitleText);
-		$(nextTitle).append(nextTitleText);
+			nextTitle.setAttribute('id', 'nextTitle');
+			nextTitle.className = 'libreNavTitle';
 
-		$(backButton).hover(function () {
-			$(backTitle).css('display', 'flex').fadeIn(200);
-		}, function () {
-			$(backTitle).css('display', 'none').fadeOut(200);
-		});
+			nextTitleText.setAttribute('id', 'nextTitleText');
+			nextTitleText.className = 'libreNavText';
+			nextTitleText.innerText = nextPageTitle;
 
-		$(nextButton).hover(function () {
-			$(nextTitle).css('display', 'flex').fadeIn(200);
-		}, function () {
-			$(nextTitle).css('display', 'none').fadeOut(200);
-		});
+			$(nextButton).html(`<i id='nextButtonIcon' class='libreNavIcon fa fa-arrow-right'></i>`);
 
-		document.body.append(backButton);
-		document.body.append(backTitle);
-		document.body.append(nextButton);
-		document.body.append(nextTitle);
+			$(nextTitle).append(nextTitleText);
+
+			$(nextButton).hover(function () {
+				$(nextTitle).css('display', 'flex').fadeIn(200);
+			}, function () {
+				$(nextTitle).css('display', 'none').fadeOut(200);
+			});
+			document.body.append(nextButton);
+			document.body.append(nextTitle);
+		}
 	}
 }
