9a5596d9178c85e3e345338fb4141f8fdb4552fb
Yasin Dahi <ymdahi@gmail.com>
2022-04-22 11:23:35 -0400

    defer to load

--- public/Miscellaneous/readerView.js
+++ public/Miscellaneous/readerView.js
@@ -1,23 +1,25 @@
 (function () {
+
+  window.addEventListener('load', function() {
+    if (window !== window.top || window.location.href.includes("readerView") || window.location.href.includes("readerView")) {
+
+      // add the readerView class to the body element (used in readerView.css)
+      document.body.classList.add("readerView");
   
-  if (window !== window.top || window.location.href.includes("readerView") || window.location.href.includes("readerView")) {
-
-    // add the readerView class to the body element (used in readerView.css)
-    document.body.classList.add("readerView");
-
-    // add the readerView parameter to specific URLs on page
-    // to ensure user does not accidently nav away from readerView
-    
-    //let pageLinks = document.querySelectorAll("article a");
-    let pageLinks = [
-      ".libreNavBtn",
-      ".mt-icon-previous-article",
-      ".mt-icon-next-article"
-    ]
-
-    pageLinks.forEach(function(el){
-      document.querySelector(el).href += "?readerView";
-    });
-  }
+      // add the readerView parameter to specific URLs on page
+      // to ensure user does not accidently nav away from readerView
+  
+      //let pageLinks = document.querySelectorAll("article a");
+      let pageLinks = [
+        ".libreNavBtn",
+        ".mt-icon-previous-article",
+        ".mt-icon-next-article"
+      ]
+  
+      pageLinks.forEach(function(el){
+        document.querySelector(el).href += "?readerView";
+      });
+    }
+  });
   
 })();
