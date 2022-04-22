77ef3dec8009c55a85e3eba76b4def9f36f12e58
Yasin Dahi <ymdahi@gmail.com>
2022-04-22 16:15:12 -0400

    typo

--- public/Miscellaneous/readerView.js
+++ public/Miscellaneous/readerView.js
@@ -4,32 +4,32 @@
 
     // add the readerView class to the body element (used in readerView.css)
     document.body.classList.add('readerView');
-  }
   
-  window.addEventListener('load', function() {
+    window.addEventListener('load', function() {
 
-    // add the readerView parameter to specific URLs on page
-    // to ensure user does not accidently nav away from readerView
+      // add the readerView parameter to specific URLs on page
+      // to ensure user does not accidently nav away from readerView
 
-    //let pageLinks = document.querySelectorAll('article a');
-    let pageLinks = [
-      'a.internal',
-      'a[rel="internal"]',
-      '#nextButton',
-      '#backButton',
-      '.mt-icon-previous-article',
-      '.mt-icon-next-article'
-    ];
+      //let pageLinks = document.querySelectorAll('article a');
+      let pageLinks = [
+        'a.internal',
+        'a[rel="internal"]',
+        '#nextButton',
+        '#backButton',
+        '.mt-icon-previous-article',
+        '.mt-icon-next-article'
+      ];
 
-    pageLinks.forEach(function(sel){
-      let el = document.querySelectorAll(sel)
-      el.forEach(function(target){
-        if (target.href.includes('libretexts.org')){
-          target.href += '?readerView';
-        }
+      pageLinks.forEach(function(sel){
+        let el = document.querySelectorAll(sel)
+        el.forEach(function(target){
+          if (target.href.includes('libretexts.org')){
+            target.href += '?readerView';
+          }
+        });
       });
-    });
 
-  });
+    });
+  }
   
 })();
