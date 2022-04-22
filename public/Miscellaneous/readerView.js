456c22295cd0b5430db5cbd6015cd6760ace5899
Yasin Dahi <ymdahi@gmail.com>
2022-04-22 15:59:51 -0400

    styling readerView'

--- public/Miscellaneous/readerView.js
+++ public/Miscellaneous/readerView.js
@@ -1,9 +1,9 @@
 (function () {
   
-  if (window !== window.top || window.location.href.includes("readerView") || window.location.href.includes("readerView")) {
+  if (window !== window.top || window.location.href.includes('readerView') || window.location.href.includes('readerView')) {
 
     // add the readerView class to the body element (used in readerView.css)
-    document.body.classList.add("readerView");
+    document.body.classList.add('readerView');
   }
   
   window.addEventListener('load', function() {
@@ -11,20 +11,21 @@
     // add the readerView parameter to specific URLs on page
     // to ensure user does not accidently nav away from readerView
 
-    //let pageLinks = document.querySelectorAll("article a");
+    //let pageLinks = document.querySelectorAll('article a');
     let pageLinks = [
-      "a.internal",
-      "#nextButton",
-      "#backButton",
-      ".mt-icon-previous-article",
-      ".mt-icon-next-article"
-    ]
+      'a.internal',
+      'a[rel="internal"]',
+      '#nextButton',
+      '#backButton',
+      '.mt-icon-previous-article',
+      '.mt-icon-next-article'
+    ];
 
     pageLinks.forEach(function(sel){
       let el = document.querySelectorAll(sel)
       el.forEach(function(target){
-        if (target.href.includes("libretexts.org")){
-          target.href += "?readerView";
+        if (target.href.includes('libretexts.org')){
+          target.href += '?readerView';
         }
       });
     });
