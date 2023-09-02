b3590c47a5df9f66d63d59a7d1dfe40c2a30da8e
Ethan Turner <ethanaturner13@gmail.com>
2023-09-02 13:38:15 -0700

    fix(reuse): instantly invoked openSandbox function

--- public/Miscellaneous/copyNavigation.js
+++ public/Miscellaneous/copyNavigation.js
@@ -136,7 +136,7 @@
             let copy = original.cloneNode(true);
             let copyTarget = copy.getElementsByTagName("a")[0];
             copyTarget.href = '#';
-            copyTarget.onclick = LibreTexts.openSandbox();
+            copyTarget.onclick = LibreTexts.openSandbox;
             copyTarget.innerText = "Your Sandbox";
             copyTarget.classList.add("mt-icon-select-all");
             copyTarget.classList.remove("mt-icon-my-contributions");
