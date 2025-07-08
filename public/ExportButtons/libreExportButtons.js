36d8dfb49c35b19b7fb5e8666a0703273a001f0d
Jake Turner <jturner@cosmistack.com>
2025-07-07 22:34:21 -0700

    fix(ExportButtons): print copy link

--- public/ExportButtons/libreExportButtons.js
+++ public/ExportButtons/libreExportButtons.js
@@ -637,7 +637,7 @@ if (!(navigator.webdriver || window.matchMedia('print').matches) && !LibreTexts?
           linkRoot = `${linkRoot}/${downloadEntry.zipFilename.replace('/Full.pdf', '')}`;
         }
 
-        const bookstoreURL = `https://commons.libretexts.org/store/product/${currentSubdomain}-${currentCoverpage.id}/${downloadEntry.zipFilename}`;
+        const bookstoreURL = `https://commons.libretexts.org/store/product/${currentSubdomain}-${currentCoverpage.id}`;
 
         const downloadOptions = [
           {
